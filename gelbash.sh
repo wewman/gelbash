8584a28e3e803daf85471cd6a5402e984955e971
wewman <wewman@users.noreply.github.com>
2017-07-01 08:22:21 -0700

    Update and Documentation

--- gelbash.sh
+++ gelbash.sh
@@ -56,23 +56,21 @@ while true; do
     #   but will get lost due to wget output
     echo -n "$pid" ' '
 
-    # Command in the variable to be used by eval
-    #   TODO Make it so it saves it to a file
-    #     and evaluates from that file
-    #     because this is inefficient,
-    #     evaling the command more than once, that is.
     # What it does:
     #  1 Gets the XML document with the given tags
     #  2 Greps out the line with file_url with its random
     #     numbers and directories so there are no duplicates
     #  3 Cuts the file_url=" from the beginning of every line
     #  4 Appends https: in the beginning of every line
+    #  5 Put everything to a file so wget can download them
+    #     NOTE Every file has 100 links
+    #       due to Gelbooru's max limit being 100
+    #       so, every 10 files is 1000 images downloaded
     get=$(curl -s "https://gelbooru.com/index.php?page=dapi&s=post&tags=$tags&q=index&pid=$pid" \
         | grep -ioE "file_url=\"\/\/assets\.gelbooru\.com\/images\/.{1,3}\/.{1,3}\/.{32}\.(jpg|png|jpeg|webm|gif)" \
         | cut -c11- \
         | sed -e "s/^/https:/" \
         | tee image_$pid.files)
-#        > image_$pid.files"
 
     # Check if the output is alive.
     if [[ ! ${get} ]]; then
@@ -83,12 +81,6 @@ while true; do
         rm image_*
         break;
     else
-        # saves the URLs in an appropriate file
-        #   NOTE Every file has 100 links
-        #     due to Gelbooru's max limit being 100
-        #     so, every 10 files is 1000 images downloaded
-        #"$get" > image_$pid.files
-
         # Downloads the files to an appropriate directory
         wget -nc -P $tags/ -c -i image_$pid.files
 
